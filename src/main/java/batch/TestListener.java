ea0cc405f6bc3e7f7d5b83072b026dea270b054b
张大川 <dachuanz@gmail.com>
2016-11-30 17:19:42 +0800

    Create TestListener.java

---
+++ src/main/java/batch/TestListener.java
@@ -0,0 +1,30 @@
+package batch;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+/**
+ * 
+ * @author zdc524
+ *  监听器 实现 JobEx  JobExecutionListener
+ */
+public class TestListener implements JobExecutionListener  {
+
+	@Override
+	public void beforeJob(JobExecution jobExecution) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterJob(JobExecution jobExecution) {
+		if( jobExecution.getStatus() == BatchStatus.COMPLETED ){
+	      System.out.println("成功");
+	    }
+	    else if(jobExecution.getStatus() == BatchStatus.FAILED){
+	        //job failure
+	    }
+
+	}
+
+}
